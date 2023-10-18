b86be131ba18edc222c3b4cde6b6bdd8c61da034
FPSgamer31 <punaisebonsang@gmail.com>
2023-10-18 18:48:52 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -38,9 +40,39 @@ public class Romain {
 		return "Romain [nom=" + nom + ", force=" + force + "]";
 	}
 	
+	public void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement]=equipement;
+		nbEquipement+=1;
+		System.out.println("le soldat " + nom + " s'équipe avec un " + equipement);
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch(nbEquipement) {
+		case 0:
+			ajouterEquipement(equipement);
+		    break;
+		case 1:
+			if (equipements[0]==equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement +" !");
+			} else {
+				ajouterEquipement(equipement);
+			}
+			break;
+		case 2:
+			System.out.println("le soldat " + nom + " est deja bien protege");
+			break;
+		default:
+			System.out.println("Il y a un soucis");
+			break;
+			
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain BigusDickus = new Romain("BigusDickus", 6);
-		System.out.println(BigusDickus); 
-		BigusDickus.recevoirCoup(5);
+        BigusDickus.sEquiper(Equipement.CASQUE);
+		BigusDickus.sEquiper(Equipement.CASQUE);
+		BigusDickus.sEquiper(Equipement.BOUCLIER);
+		BigusDickus.sEquiper(Equipement.BOUCLIER);
 	}
 }
